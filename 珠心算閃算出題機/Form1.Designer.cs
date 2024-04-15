ffccbd18ab8b19debae4252f737c0e01bc590684
ice175848 <memory509609@gmail.com>
2024-04-16 00:17:58 +0800

    Initial commit

---
+++ 珠心算閃算出題機/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿using System;
+
+namespace 珠心算閃算出題機
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(681, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "出題";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "間隔(毫秒)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "口數";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(372, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "位數";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.radioButton1);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton2);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton3);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton4);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton5);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(527, 7);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(138, 89);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "只有加";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 25);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(115, 16);
+            this.radioButton2.TabIndex = 0;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "加減混合(真補數)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 47);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(115, 16);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "加減混合(假補數)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(3, 69);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 16);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "乘法";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(56, 69);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(47, 16);
+            this.radioButton5.TabIndex = 0;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "除法";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 50F);
+            this.label4.Location = new System.Drawing.Point(67, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(253, 67);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Question";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(79, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1000;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(240, 14);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 11;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(407, 15);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(114, 22);
+            this.numericUpDown3.TabIndex = 12;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(681, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1001;
+            this.button2.Text = "Answer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(454, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 12);
+            this.label5.TabIndex = 1002;
+            this.label5.Text = "label5";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(681, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1003;
+            this.button3.Text = "顯示題目";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(762, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 23);
+            this.button4.TabIndex = 1004;
+            this.button4.Text = "再來一次(尚未開放)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Flash";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 珠心算閃算出題機/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Threading; //thread要用
+using Timer = System.Timers.Timer;
+
+namespace 珠心算閃算出題機
+{
+    public partial class Form1 : Form
+    {
+        private delegate void DelShowMessage(string sMessage);
+        private void AddMessage(string sMessage)
+        {
+            if (this.InvokeRequired) // 若非同執行緒
+            {
+                DelShowMessage del = new DelShowMessage(AddMessage); //利用委派執行
+                this.Invoke(del, sMessage);
+            }
+            else // 同執行緒
+            {
+
+                this.label4.Text = sMessage + Environment.NewLine;
+                label5.Text = times_check.ToString();
+                if (times_check == times || mode == 4 || mode == 5)
+                {
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                }
+            }
+        }
+        public void slow(object sender = null, EventArgs e = null)
+        {
+            timer.Stop();
+            AddMessage("");
+            //Thread.Sleep(100);
+            timer1.Stop();
+            timer1.Enabled = false;
+            timer.Start();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            rnd = new Random();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int min_value;
+        int max_value;
+        int mode = 1;
+        int times;
+        int times_check = 0;
+        int cycle=0;
+        int hard;
+        int ans;
+        int random;
+        int time_i;
+        int again_i = 0;
+        Random rnd;
+        int[] memory;
+
+        Timer timer = new Timer();
+        //Timer pause = new Timer();
+        public void button1_Click(object sender, EventArgs ee)
+        {
+            time_i = 0;
+            ans = 0;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            label4.Text = "";
+            cycle = int.Parse(numericUpDown1.Text);//週期
+            times = int.Parse(numericUpDown2.Text);
+            times_check = 0;
+            hard = int.Parse(numericUpDown3.Text);
+            memory = new int[times];
+            max_value = Convert.ToInt32(Math.Pow(10, hard));
+            if (radioButton1.Checked == true)
+            {
+                mode = 1;
+            }
+            if (radioButton2.Checked == true)
+            {
+                mode = 2;
+            }
+            if (radioButton3.Checked == true)
+            {
+                mode = 3;
+            }
+            if (radioButton4.Checked == true)
+            {
+                mode = 4;
+            }
+            if (radioButton5.Checked == true)
+            {
+                mode = 5;
+            }
+            if(mode == 1||mode == 2||mode == 3)
+            {
+                timer = new Timer();
+                timer.Enabled = true;
+                timer.Interval = cycle;  // 執行區隔時間
+                timer.Start();
+                timer.Elapsed += new ElapsedEventHandler(test);
+            }
+            if(mode ==4||mode == 5)
+            {
+                test();
+            }
+        }
+        public void test(object source=null,ElapsedEventArgs e=null)
+        {
+            if (times_check != times)
+            {
+                times_check++;
+                
+                switch (mode)
+                {
+                    case 1://只有加
+                        {
+                            //pause.Start();
+                            timer1.Tick += new EventHandler(slow);
+                            timer1.Start();
+                            int random = rnd.Next(1, max_value);
+                            memory[time_i] = random;
+                            time_i++;
+                            ans += random;
+                            AddMessage(random.ToString());
+                            //label4.Text = random.ToString();
+                        }
+                        break;
+                    case 2://加減混合
+                        {
+                            int random = rnd.Next(0-max_value, max_value);
+                            
+                            while (ans+random<0)
+                            {
+                                random = rnd.Next(0 - max_value, max_value);
+                            }
+                            memory[time_i] = random;
+                            time_i++;
+                            ans += random;
+                            AddMessage(random.ToString());
+                        }
+                        break;
+                    case 3:
+                        {
+                            int random = rnd.Next(0 - max_value, max_value);
+                            memory[time_i] = random;
+                            time_i++;
+                            ans += random;
+                            AddMessage(random.ToString());
+                        }
+                        break;
+                    case 4:
+                        {
+                            int questionX = rnd.Next(Convert.ToInt32(Math.Pow(10, times-1)), Convert.ToInt32(Math.Pow(10, times)));
+                            int questionY = rnd.Next(Convert.ToInt32(Math.Pow(10, hard-1)), Convert.ToInt32(Math.Pow(10, hard)));
+                            ans = questionX * questionY;
+                            string str = ($"{questionX}×{questionY}=?");
+                            AddMessage(str);
+                        }
+                        break;
+                    case 5:
+                        {
+                            int ansX = rnd.Next(Convert.ToInt32(Math.Pow(10, times - hard)), Convert.ToInt32(Math.Pow(10, times - hard + 1)));
+                            int questionY = rnd.Next(Convert.ToInt32(Math.Pow(10, hard - 1)), Convert.ToInt32(Math.Pow(10, hard)));
+                            while(ansX * questionY>Math.Pow(10,times))
+                            {
+                                ansX = rnd.Next(Convert.ToInt32(Math.Pow(10, times - hard)), Convert.ToInt32(Math.Pow(10, times - hard + 1)));
+                            }
+                            int questionX = ansX * questionY;
+                            string str = ($"{questionX}÷{questionY}=?");
+                            ans = ansX;
+                            AddMessage(str);
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                timer.Enabled = false;
+                timer.Stop();
+                //pause.Enabled = false;
+                //pause.Stop();
+            }
+        }
+        public void again(object source = null, ElapsedEventArgs e = null)
+        {
+            if (again_i < times)
+            {
+                AddMessage(memory[again_i].ToString());
+                again_i++;
+            }
+            else
+            {
+                again_i = 0;
+                timer.Enabled = false;
+                timer.Stop();
+                //pause.Stop();
+            }
+        }
+        private void domainUpDown3_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label5.Text = ans.ToString();
+        }
+
+        
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked == true)
+            {
+                numericUpDown1.Enabled = false;
+            }
+        }
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton5.Checked == true)
+            {
+                numericUpDown1.Enabled = false;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label5.Text = null;
+            for (int i = 0; i < times; i++)
+                label5.Text += "\n"+memory[i];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cycle = int.Parse(numericUpDown1.Text);//週期
+
+            timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = cycle;  // 執行區隔時間
+            timer.Start();
+            timer.Elapsed += new ElapsedEventHandler(again);
+
+        }
+    }
+}
